2c0eff409a29b985bc7b56409eb18e005dbdc235
Gilderson Santos <122750614+gildersonsantos@users.noreply.github.com>
2024-05-05 15:48:43 -0300

    Atualização de Script

--- js/script.js
+++ js/script.js
@@ -11,3 +11,12 @@ btnMenu.addEventListener("click", () => {
     }
 })
 
+const btnView = document.querySelector("main button.view");
+btnView.addEventListener("click", () => {
+    window.location.href = "../pages/visualiza-livro.html";
+})
+
+const btnCaretLeft = document.querySelector("header button.caret-left");
+btnCaretLeft.addEventListener("click", () => {
+    window.history.back()
+})
