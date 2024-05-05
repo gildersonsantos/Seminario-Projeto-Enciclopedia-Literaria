394a37ea4d8e8f9cac2f2c9259cf38803e32e2f5
Gilderson Santos <122750614+gildersonsantos@users.noreply.github.com>
2024-05-05 02:38:08 -0300

    Atualização do Design Responsivo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const btnMenu = document.querySelector("header button.list");
+const aside = document.querySelector("aside");
+
+btnMenu.addEventListener("click", () => {
+    if (aside.classList.contains("active")) {
+        aside.classList.remove("active");
+        aside.classList.add("reverse");
+    } else {
+        aside.classList.add("active");
+        aside.classList.remove("reverse");
+    }
+})
+
