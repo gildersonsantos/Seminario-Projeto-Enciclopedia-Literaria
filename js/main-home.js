4e17901de5cc145ebb4449a88eff4370eba21163
Gilderson Santos <122750614+gildersonsantos@users.noreply.github.com>
2024-05-06 11:18:24 -0300

    Correção de bugs e criação da pagina editar livro

--- js/main-home.js
+++ js/main-home.js
@@ -3,6 +3,16 @@ btnView.addEventListener("click", () => {
     window.location.href = "../pages/visualiza-livro.html";
 })
 
+const btnEdit = document.querySelector("main button.edit");
+btnEdit.addEventListener("click", () => {
+    window.location.href = "../pages/editar-livro.html";
+})
+
+const btnDelete= document.querySelector("main button.delete");
+btnDelete.addEventListener("click", () => {
+    alert("Deu ruim, falta fazer!");
+})
+
 const btnAddNewLibre = document.querySelector("main button.add-new-libre");
 btnAddNewLibre.addEventListener("click", () => {
     window.location.href = "../pages/adiciona-livro.html";
